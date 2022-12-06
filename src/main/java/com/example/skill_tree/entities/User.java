250e56e646aa4def99d6857438b6ce0b087c2139
Elmehdi <93981356+Raikonerr@users.noreply.github.com>
2022-12-06 09:40:34 +0100

    build : user entity

--- src/main/java/com/example/skill_tree/entities/User.java
+++ src/main/java/com/example/skill_tree/entities/User.java
@@ -14,6 +14,56 @@ public class User{
     @Column(name = "username")
     @Basic
     private String username;
+    @Column(name = "email")
+    @Basic
+    private String email;
+    @Column(name = "password")
+    @Basic
+    private String password;
+    @Column(name = "image")
+    @Basic
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
 
 
 
